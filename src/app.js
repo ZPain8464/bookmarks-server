6cac9d357f1c59c508ba8703c2ba41dca51e1fae
Zach Painter <z.painter45@gmail.com>
2020-10-12 12:56:21 -0500

    fixed validation issues

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
-const bookmarks = require("./store");
-const { v4: uuid } = require("uuid");
 const bookmarksRouter = require("./bookmarks/bookmarks-router");
+const validateBearerToken = require("./validateToken");
+const logger = require("./logger");
 
 const app = express();
 
@@ -15,6 +15,7 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+app.use(validateBearerToken);
 app.use(bookmarksRouter);
 
 app.get("/", (req, res) => {
@@ -26,21 +27,10 @@ app.use(function errorHandler(error, req, res, next) {
   if (NODE_ENV === "product") {
     response = { error: { message: "server error" } };
   } else {
-    console.log(error);
     response = { message: error.message, error };
   }
+  logger.error(error);
   res.status(500).json(response);
-});
-
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get("Authorization");
-
-  if (!authToken || authToken.split(" ")[1] !== apiToken) {
-    logger.error(`Unauthorized request to path: ${req.path}`);
-    return res.status(401).json({ error: "Unauthorized request" });
-  }
-  next();
 });
 
 module.exports = app;
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -13,7 +13,7 @@ bookmarksRouter
     res.json(store.bookmarks);
   })
   .post(bodyParser, (req, res) => {
-    for (const field of ["title", "url", "rating", "desc"]) {
+    for (const field of ["title", "url", "rating"]) {
       if (!req.body[field]) {
         logger.error(`${field} is required`);
         return res.status(400).send(`'${field}' is required`);
